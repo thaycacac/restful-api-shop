982ac3bc4a2cb5529282cb51deef61f2662b25f2
thaycacac <thaycacac@gmail.com>
2018-07-24 01:11:37 +0700

    mongoose and setup

--- restful-api-shop/api/routes/products.js
+++ restful-api-shop/api/routes/products.js
@@ -1,46 +1,107 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-      message: 'Handing GET requests to /products'
+  Product.find()
+  .exec()
+  .then(doc => {
+    console.log(doc)
+    if(doc.length >= 0) {
+      res.status(200).json(doc)
+    } else {
+      res.status(404).json({
+        message: 'No entries found'
+      })
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      error: err
+    })
   })
 })
 
-// router.post('/', (req, res, next) => {
-//   const product = {
-//     name: req.body.name,
-//     price: req.body.price
-//   }
-//   res.status(201).json({
-//     message: 'Handing POST requests to /products',
-//     createdProduct: product
-//   })
-// })
+router.post('/', (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  })
+  product.save()
+  .then(result => {
+    console.log(result)
+    res.status(201).json({
+      message: 'Handing POST requests to /products',
+      createdProduct: result
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      error: err
+    })
+  })
+})
 
-// router.get('/:productId', (req, res, next) => {
-//   const id = req.params.productId
-//   if(id === 'special') {
-//     res.status(200).json({
-//       message: 'You discovered the special ID'
-//     })
-//   } else {
-//     res.status(200).json({
-//       message: 'You passed an ID'
-//     })
-//   }
-// })
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId
+  Product.findById(id)
+  .exec()
+  .then(doc => {
+    console.log("From dataase", doc)
+    if (doc) {
+      res.status(200).json(doc)
+    } else {
+      res.status(404).json({
+        message: 'No valid entry found for provied ID'
+      })
+    }
+  })
+  .catch(err => {
+    // console.log(err)
+    res.status(500).json({
+      error: err
+    })
+  })
+})
 
-// router.patch('/:productId', (req, res, next) => {
-//   res.status(200).json({
-//     message: 'Updated product!'
-//   })
-// })
+router.patch('/:productId', (req, res, next) => {
+  const id = req.params.productId
+  const updateOps = {}
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value
+  }
+  Product.update({_id: id}, { $set: updateOps})
+  .exec()
+  .then(result => {
+    console.log(result)
+    res.status(200).json(result)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      error: err
+    })
+  })
+})
 
-// router.delete('/:productId', (req, res, next) => {
-//     res.status(200).json({
-//       message: 'Deleted product!'
-//     })
-//   })
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId
+    Product.remove({_id: id})
+    .exec()
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    })
+  })
 
 module.exports = router
--- restful-api-shop/app.js
+++ restful-api-shop/app.js
@@ -2,22 +2,32 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://hoapnse05740:Camonem123@ds239071.mlab.com:39071/guitar-tabs', {
+  useNewUrlParser: true
+}).then(() => {
+  console.log('Database connected')
+}, err => {
+  console.log('Can not connect on database', err)
+})
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  if(req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-    return res.status(200).json({})
-  }
-})
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin')
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+//   if(req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+//     return res.status(200).json({})
+//   }
+// })
 
 //Routes which should handle requests 
 app.use('/products', productRoutes)
