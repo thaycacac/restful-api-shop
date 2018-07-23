49b1a01b0e88f69e1d2828c0da95a80070307c31
thaycacac <thaycacac@gmail.com>
2018-07-23 15:00:01 +0700

    Handling Errors & Improving the Project Setup

--- restful-api-shop/api/routes/orders.js
+++ restful-api-shop/api/routes/orders.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+//Hander incoming GET requests to /order
 router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Orders were fetched'
