4c7b887da244613b845ec8d2ecf4238babd6b7ac
thaycacac <thaycacac@gmail.com>
2018-07-23 16:32:04 +0700

    Parsing the Body & Handling CORS

--- restful-api-shop/api/routes/products.js
+++ restful-api-shop/api/routes/products.js
@@ -2,15 +2,20 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handing GET requests to /products'
-    })
+  res.status(200).json({
+      message: 'Handing GET requests to /products'
+  })
 })
 
 router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'Handing POST requests to /products'
-    })
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  }
+  res.status(201).json({
+    message: 'Handing POST requests to /products',
+    createdProduct: product
+  })
 })
 
 router.get('/:productId', (req, res, next) => {
